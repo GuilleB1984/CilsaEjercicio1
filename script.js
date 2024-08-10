a6b3598d694ee2055b24935b651c7d9f8ec42d8f
Guillermo Belloni <guillermo.belloni@gmail.com>
2024-08-10 16:57:39 -0300

    Actualizacion JS

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("datosFormulario").addEventListener("submit", function(event) {
     var email = document.getElementById("email").value;
+    
     var emailValidacion = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
 
     if (!emailValidacion.test(email)) {
         alert("Por favor, introduce un correo electrónico válido.");
